b0cf7f0421cac56003677c0a17b2b6b051546e2e
Brandon Dutra <brandondutra@google.com>
2017-05-07 13:59:58 -0700

    training from csv + target working with gcloud local predict

--- solutionbox/analyze/trainer/task.py
+++ solutionbox/analyze/trainer/task.py
@@ -21,6 +21,7 @@ import pandas as pd
 import re
 import six
 import sys
+import math
 import multiprocessing
 import json
 
@@ -36,6 +37,28 @@ from tensorflow_transform.tf_metadata import metadata_io
 
 from tensorflow_transform.saved import saved_transform_io
 from tensorflow_transform.tf_metadata import dataset_schema
+
+
+from tensorflow.contrib.learn.python.learn.utils import input_fn_utils
+from tensorflow.contrib.learn.python.learn import export_strategy
+from tensorflow.contrib.learn.python.learn.utils import (
+    saved_model_export_utils)
+
+from tensorflow.python.ops import variables
+from tensorflow.contrib.framework.python.ops import variables as contrib_variables
+from tensorflow.contrib.learn.python.learn.estimators import model_fn as model_fn_lib
+from tensorflow.python.training import saver
+from tensorflow.python.ops import data_flow_ops
+from tensorflow.python.framework import ops
+from tensorflow.python.client import session as tf_session
+from tensorflow.python.saved_model import builder as saved_model_builder
+from tensorflow.python.saved_model import tag_constants
+from tensorflow.python.ops import control_flow_ops
+from tensorflow.python.util import compat
+from tensorflow.python.platform import gfile
+from tensorflow.python.saved_model import signature_def_utils
+
+
 
 # Files
 SCHEMA_FILE = 'schema.json'
@@ -61,6 +84,17 @@ TARGET_TRANSFORM = 'target'
 NUMERIC_TRANSFORMS = [IDENTITY_TRANSFORM, SCALE_TRANSFORM]
 CATEGORICAL_TRANSFORMS = [ONE_HOT_TRANSFORM, EMBEDDING_TRANSFROM]
 TEXT_TRANSFORMS = [BOW_TRANSFORM, TFIDF_TRANSFORM]
+
+
+# Constants for the Prediction Graph fetch tensors.
+PG_TARGET = 'target'  # from input
+
+PG_REGRESSION_PREDICTED_TARGET = 'predicted'
+
+PG_CLASSIFICATION_FIRST_LABEL = 'predicted'
+PG_CLASSIFICATION_FIRST_SCORE = 'score'
+PG_CLASSIFICATION_LABEL_TEMPLATE = 'predicted_%s'
+PG_CLASSIFICATION_SCORE_TEMPLATE = 'score_%s'
 
 def parse_arguments(argv):
   """Parse the command line arguments."""
@@ -236,6 +270,324 @@ def build_feature_columns(features, stats, model_type):
   return feature_columns
 
 
+def _recursive_copy(src_dir, dest_dir):
+  """Copy the contents of src_dir into the folder dest_dir.
+  Args:
+    src_dir: gsc or local path.
+    dest_dir: gcs or local path.
+  When called, dest_dir should exist.
+  """
+
+  file_io.recursive_create_dir(dest_dir)
+  for file_name in file_io.list_directory(src_dir):
+    old_path = os.path.join(src_dir, file_name)
+    new_path = os.path.join(dest_dir, file_name)
+
+    if file_io.is_directory(old_path):
+      _recursive_copy(old_path, new_path)
+    else:
+      file_io.copy(old_path, new_path, overwrite=True)
+
+def make_prediction_output_tensors(
+          args,
+          features,
+          input_ops,
+          model_fn_ops,
+          keep_target):
+  """Makes the final prediction output layer."""
+  target_name = get_target_name(features)
+  key_name = get_key_name(features)
+
+  outputs = {}
+  outputs[key_name] = tf.squeeze(input_ops.features[key_name])
+
+  if is_classification_model(args.model_type):
+
+    # build maps from ints to the origional categorical strings.
+    string_values = read_vocab(args, target_name)
+    table = tf.contrib.lookup.index_to_string_table_from_tensor(
+        mapping=string_values,
+        default_value='UNKNOWN')
+
+    # Get the label of the input target.
+    if keep_target:
+      input_target_label = table.lookup(input_ops.features[target_name])
+      outputs[PG_TARGET] = tf.squeeze(input_target_label)
+
+    # TODO(brandondutra): get the score of the target label too.
+    probabilities = model_fn_ops.predictions['probabilities']
+
+    # get top k labels and their scores.
+    (top_k_values, top_k_indices) = tf.nn.top_k(probabilities, k=args.top_n)
+    top_k_labels = table.lookup(tf.to_int64(top_k_indices))
+
+    # Write the top_k values using 2*top_k columns.
+    num_digits = int(math.ceil(math.log(args.top_n, 10)))
+    if num_digits == 0:
+      num_digits = 1
+    for i in range(0, args.top_n):
+      # Pad i based on the size of k. So if k = 100, i = 23 -> i = '023'. This
+      # makes sorting the columns easy.
+      padded_i = str(i + 1).zfill(num_digits)
+
+      if i == 0:
+        label_alias = PG_CLASSIFICATION_FIRST_LABEL
+      else:
+        label_alias = PG_CLASSIFICATION_LABEL_TEMPLATE % padded_i
+
+      label_tensor_name = (tf.squeeze(
+          tf.slice(top_k_labels, [0, i], [tf.shape(top_k_labels)[0], 1])))
+
+      if i == 0:
+        score_alias = PG_CLASSIFICATION_FIRST_SCORE
+      else:
+        score_alias = PG_CLASSIFICATION_SCORE_TEMPLATE % padded_i
+
+      score_tensor_name = (tf.squeeze(
+          tf.slice(top_k_values,
+                   [0, i],
+                   [tf.shape(top_k_values)[0], 1])))
+
+      outputs.update({label_alias: label_tensor_name,
+                      score_alias: score_tensor_name})
+
+  else:
+    if keep_target:
+      outputs[PG_TARGET] = tf.squeeze(input_ops.features[target_name])
+
+    scores = model_fn_ops.predictions['scores']
+    outputs[PG_REGRESSION_PREDICTED_TARGET] = tf.squeeze(scores)
+
+  return outputs  
+
+# TODO(brandondutra) remove this when this in in the public tft package
+# copied from tensorflow_transform/saved/input_fn_maker.py at head
+def build_csv_transforming_serving_input_fn(
+    raw_metadata,
+    transform_savedmodel_dir,
+    raw_keys,
+    field_delim=",",
+    convert_scalars_to_vectors=True):
+  """Creates input_fn that applies transforms to raw data in csv format.
+
+  CSV files have many restrictions and are not suitable for every input source.
+  Consider using build_parsing_transforming_serving_input_fn (which is good for
+  input sources of tensorflow records containing tf.example) or
+  build_default_transforming_serving_input_fn (which is good for input sources
+  like json that list each input tensor).
+
+  CSV input sources have the following restrictions:
+    * Only columns with schema tf.FixedLenFeature colums are supported
+    * Text columns containing the delimiter must be wrapped in '"'
+    * If a string contains a double quote, the double quote must be escaped with
+      another double quote, for example: the first column in
+      '"quick ""brown"" fox",1,2' becomes 'quick "brown" fox'
+    * White space is kept. So a text column "label ," is parsed to 'label '
+
+  Args:
+    raw_metadata: a `DatasetMetadata` object describing the raw data.
+    transform_savedmodel_dir: a SavedModel directory produced by tf.Transform
+      embodying a transformation function to be applied to incoming raw data.
+    raw_keys: A list of string keys of the raw labels to be used. The order in
+      the list matches the parsing order in the csv file.
+    field_delim: Delimiter to separate fields in a record.
+    convert_scalars_to_vectors: Boolean specifying whether this input_fn should
+      convert scalars into 1-d vectors.  This is necessary if the inputs will be
+      used with `FeatureColumn`s as `FeatureColumn`s cannot accept scalar
+      inputs. Default: True.
+
+  Raises:
+    ValueError: if columns cannot be saved in a csv file.
+
+  Returns:
+    An input_fn suitable for serving that applies transforms to raw data in
+    CSV format.
+  """
+  if not raw_keys:
+    raise ValueError("raw_keys must be set.")
+
+  column_schemas = raw_metadata.schema.column_schemas
+
+  # Check for errors.
+  for k in raw_keys:
+    if k not in column_schemas:
+      raise ValueError("Key %s does not exist in the schema" % k)
+    if not isinstance(column_schemas[k].representation,
+                      dataset_schema.FixedColumnRepresentation):
+      raise ValueError(("CSV files can only support tensors of fixed size"
+                        "which %s is not.") % k)
+    shape = column_schemas[k].tf_shape().as_list()
+    if shape and shape != [1]:
+      # Column is not a scalar-like value. shape == [] or [1] is ok.
+      raise ValueError(("CSV files can only support features that are scalars "
+                        "having shape []. %s has shape %s")
+                       % (k, shape))
+  def default_transforming_serving_input_fn():
+    """Serving input_fn that applies transforms to raw data in Tensors."""
+
+    record_defaults = []
+    for k in raw_keys:
+      if column_schemas[k].representation.default_value is not None:
+        value = tf.constant([column_schemas[k].representation.default_value],
+                            dtype=column_schemas[k].domain.dtype)
+      else:
+        value = tf.constant([], dtype=column_schemas[k].domain.dtype)
+      record_defaults.append(value)
+
+    placeholder = tf.placeholder(dtype=tf.string, shape=(None,),
+                                 name="csv_input_placeholder")
+    parsed_tensors = tf.decode_csv(placeholder, record_defaults,
+                                   field_delim=field_delim)
+
+    raw_serving_features = {k: v for k, v in zip(raw_keys, parsed_tensors)}
+
+    _, transformed_features = (
+        saved_transform_io.partially_apply_saved_transform(
+            transform_savedmodel_dir, raw_serving_features))
+
+    if convert_scalars_to_vectors:
+      transformed_features = input_fn_maker._convert_scalars_to_vectors(transformed_features)
+
+    return input_fn_utils.InputFnOps(
+        transformed_features, None, {"csv_example": placeholder})
+
+  return default_transforming_serving_input_fn
+
+
+  
+
+def make_export_strategy(
+        args,
+        input_format,
+        keep_target,
+        assets_extra,
+        features,
+        schema):
+  """Makes prediction graph.
+
+  Args:
+    args: command line args
+    input_format: model input data
+        'tfex': tf.example files of transformed data
+        'csv': csv files of raw data
+    keep_target: If ture, target column is returned in prediction graph. Target
+        column must also exist in input data
+    assets_extra: other fiels to copy to the output folder
+    job_dir: root job folder
+    features: features dict
+    schema: schema list
+  """
+
+  target_name = get_target_name(features)
+  key_name = get_key_name(features)
+  raw_metadata = raw_metadata = metadata_io.read_metadata(
+        os.path.join(args.analysis_output_dir, RAW_METADATA_DIR))
+  def export_fn(estimator, export_dir_base, checkpoint_path=None, eval_result=None):
+    with ops.Graph().as_default() as g:
+      contrib_variables.create_global_step(g)
+
+      input_ops = None
+      if input_format == 'csv':
+        csv_header = [col['name'] for col in schema]
+        if not keep_target:
+          csv_header.remove(target_name)
+        input_ops= build_csv_transforming_serving_input_fn(
+            raw_metadata=raw_metadata,
+            transform_savedmodel_dir=os.path.join(args.analysis_output_dir, TRANSFORM_FN_DIR),
+            raw_keys=csv_header,
+            field_delim=",",
+            convert_scalars_to_vectors=True)()
+      elif input_format == 'tfex':
+        raise NotImplementedError('tfex')
+      else:
+        raise ValueError('Unknown input_format parameter value')
+
+      model_fn_ops = estimator._call_model_fn(input_ops.features,
+                                              None,
+                                              model_fn_lib.ModeKeys.INFER)
+      output_fetch_tensors = make_prediction_output_tensors(
+          args=args,
+          features=features,
+          input_ops=input_ops,
+          model_fn_ops=model_fn_ops,
+          keep_target=keep_target)
+
+      signature_def_map = {
+        'serving_default': signature_def_utils.predict_signature_def(input_ops.default_inputs,
+                                                                     output_fetch_tensors)
+      }
+
+      if not checkpoint_path:
+        # Locate the latest checkpoint
+        checkpoint_path = saver.latest_checkpoint(estimator._model_dir)
+      if not checkpoint_path:
+        raise NotFittedError("Couldn't find trained model at %s."
+                             % estimator._model_dir)
+
+      export_dir = saved_model_export_utils.get_timestamped_export_dir(
+          export_dir_base)
+
+      with tf_session.Session('') as session:
+        # variables.initialize_local_variables()
+        variables.local_variables_initializer()
+        data_flow_ops.tables_initializer()
+        saver_for_restore = saver.Saver(
+            variables.global_variables(),
+            sharded=True)
+        saver_for_restore.restore(session, checkpoint_path)
+
+        init_op = control_flow_ops.group(
+            variables.local_variables_initializer(),
+            data_flow_ops.tables_initializer())
+
+        # Perform the export
+        builder = saved_model_builder.SavedModelBuilder(export_dir)
+        builder.add_meta_graph_and_variables(
+            session, [tag_constants.SERVING],
+            signature_def_map=signature_def_map,
+            assets_collection=ops.get_collection(
+                ops.GraphKeys.ASSET_FILEPATHS),
+            legacy_init_op=init_op)
+        builder.save(False)
+
+      # Add the extra assets
+      if assets_extra:
+        assets_extra_path = os.path.join(compat.as_bytes(export_dir),
+                                         compat.as_bytes('assets.extra'))
+        for dest_relative, source in assets_extra.items():
+          dest_absolute = os.path.join(compat.as_bytes(assets_extra_path),
+                                       compat.as_bytes(dest_relative))
+          dest_path = os.path.dirname(dest_absolute)
+          gfile.MakeDirs(dest_path)
+          gfile.Copy(source, dest_absolute)
+
+    # only keep the last 3 models
+    saved_model_export_utils.garbage_collect_exports(
+        export_dir_base,
+        exports_to_keep=3)
+
+    # save the last model to the model folder.
+    # export_dir_base = A/B/intermediate_models/
+    if keep_target:
+      final_dir = os.path.join(args.job_dir, 'evaluation_model')
+    else:
+      final_dir = os.path.join(args.job_dir, 'model')
+    if file_io.is_directory(final_dir):
+      file_io.delete_recursively(final_dir)
+    file_io.recursive_create_dir(final_dir)
+    _recursive_copy(export_dir, final_dir)
+
+    return export_dir
+
+  if keep_target:
+    intermediate_dir = 'intermediate_evaluation_models'
+  else:
+    intermediate_dir = 'intermediate_prediction_models'
+
+  return export_strategy.ExportStrategy(intermediate_dir, export_fn)
+
+
+# TODO(brandondutra) add this to tft
 def build_csv_transforming_training_input_fn(raw_metadata,
                                              transform_savedmodel_dir,
                                              raw_data_file_pattern,
@@ -437,9 +789,22 @@ def read_vocab(args, column_name):
   vocab_str = file_io.read_file_to_string(vocab_path)
   vocab = pd.read_csv(six.StringIO(vocab_str),
                       header=None,
-                      names=['token', 'count'])
+                      names=['token', 'count'],
+                      dtype=str)  # Prevent pd from converting numerical categories.)
   return vocab['token'].tolist()
 
+
+def get_target_name(features):
+  for name, transform in six.iteritems(features):
+    if transform['transform'] == TARGET_TRANSFORM:
+      return name
+  return None
+
+def get_key_name(features):
+  for name, transform in six.iteritems(features):
+    if transform['transform'] == KEY_TRANSFORM:
+      return name
+  return None
 
 def gzip_reader_fn():
   return tf.TFRecordReader(options=tf.python_io.TFRecordOptions(
@@ -472,14 +837,9 @@ def get_experiment_fn(args):
       raise ValueError('File not found: %s' % stats_file_path)
     stats = json.loads(file_io.read_file_to_string(stats_file_path).decode())
 
-    target_column_name = None
-    key_column_name = None
+    target_column_name = get_target_name(features)
+    key_column_name = get_key_name(features)
     header_names = [col['name'] for col in schema]
-    for name, transform in six.iteritems(features):
-      if transform['transform'] == TARGET_TRANSFORM:
-        target_column_name = name
-      elif transform['transform'] == KEY_TRANSFORM:
-        key_column_name = name
     if not target_column_name or not key_column_name:
       raise ValueError('target or key transform missing from features file.') 
     
@@ -492,6 +852,13 @@ def get_experiment_fn(args):
     additional_assets = {FEATURES_FILE: features_file_path,
                          SCHEMA_FILE: schema_file_path}
 
+    export_strategy_tfex_target = make_export_strategy(
+        args=args,
+        input_format='csv',
+        keep_target=True,
+        assets_extra=additional_assets,
+        features=features,
+        schema=schema)
     #export_strategy_target = util.make_export_strategy(
     #    train_config=train_config,
     #    args=args,
@@ -512,7 +879,7 @@ def get_experiment_fn(args):
 
       input_reader_for_train = build_csv_transforming_training_input_fn(
           raw_metadata=raw_metadata,
-          transform_savedmodel_dir=os.path.join(args.analysis_output_dir, 'transform_fn'),
+          transform_savedmodel_dir=os.path.join(args.analysis_output_dir, TRANSFORM_FN_DIR),
           raw_data_file_pattern=args.train_data_paths,
           training_batch_size=args.train_batch_size,
           raw_keys=header_names,
@@ -525,7 +892,7 @@ def get_experiment_fn(args):
       )
       input_reader_for_eval = build_csv_transforming_training_input_fn(
           raw_metadata=raw_metadata,
-          transform_savedmodel_dir=os.path.join(args.analysis_output_dir, 'transform_fn'),
+          transform_savedmodel_dir=os.path.join(args.analysis_output_dir, TRANSFORM_FN_DIR),
           raw_data_file_pattern=args.eval_data_paths,
           training_batch_size=args.eval_batch_size,
           raw_keys=header_names,
@@ -568,7 +935,7 @@ def get_experiment_fn(args):
         train_input_fn=input_reader_for_train,
         eval_input_fn=input_reader_for_eval,
         train_steps=args.max_steps,
-        #export_strategies=[export_strategy_target, export_strategy_notarget],
+        export_strategies=[export_strategy_tfex_target],
         min_eval_frequency=args.min_eval_frequency,
         eval_steps=None,
     )
